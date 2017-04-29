f2be063cfca56da6f55270c02e4ee59e79233bf7
brittanywalker <brittwalker@outlook.com>
2017-04-29 17:13:16 +1200

    Test file with if statement

--- obfuscation/test.java
+++ obfuscation/test.java
@@ -4,11 +4,13 @@ private static double fullnessLevel = 8;
 
 public static double getFullnessLevel() {
      return fullnessLevel;
-   }    
+   }
 
 public static void setFullnessLevel(double fullnessLevel) {
+  if(1>0){
        this.fullnessLevel = fullnessLevel;
      }
+   }
 
 public static double getFullnessLevel() {
          return fullnessLevel;
