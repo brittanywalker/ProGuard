5b107eefd07967017077e57f3d63ef7769bb5969
brittanywalker <brittwalker@outlook.com>
2017-04-30 11:56:59 +1200

    Opaque predicates added

--- obfuscation/obfuscateScript.py
+++ obfuscation/obfuscateScript.py
@@ -7,27 +7,15 @@ f = open(filename, "r")
 lines = f.readlines()
 f.close()
 
-lines.insert(len(lines)-2, "public static int whiteNoise(int a, int b) {int c = 0; c = a+b; return a;}")
+lines.insert(len(lines)-2, "public static int whiteNoise(int a, int b) {int c = 0; c = a+b; int d = whiteNoise2(c, a, b); return a;}\
+ public static int whiteNoise2(int a, int b, int c){int d = a+b/c; return a;}")
 
 f = open(filename, "w")
 lines = "".join(lines)
+lines = lines.replace("if (", "if (1>0 && ")
+lines = lines.replace("if(", "if (1>0 && ")
 lines = lines.replace("\n", "")
 lines = lines.replace("\t", "")
 
 f.write(lines)
 f.close()
-
-'''
-	for i in range(len(lines)):
-		if i == len(lines) -2:
-			f.write("here")
-
-for line in fileinput.input(filename, inplace=True):
-	if "get" in line:
-		line = line.replace("get", "g")
-	if "set" in line:
-		line = line.replace("set", "s")
-
-	sys.stdout.write(line)
-
-'''
