950704b4ee2e3f61520c24befdd4a10712be7bb7
Harry Jackson <hjac660@aucklanduni.ac.nz>
2017-04-30 10:55:44 +1200

    Working encryption

--- obfuscation/Java/ObfucatorTool/src/Obfuscator.java
+++ obfuscation/Java/ObfucatorTool/src/Obfuscator.java
@@ -4,25 +4,30 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.*;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * Created by harry on 29/04/2017.
  */
 public class Obfuscator {
     public static void main(String[] args){
-
+        //TODO run python for each file, and auto find XML file / strings
         runPythonScript("test.java");
         encryptXML();
 
@@ -53,13 +58,9 @@ public class Obfuscator {
                 Node node = stringList.item(i);
                 if (node.getNodeName().equals("string")){
                     String s = node.getTextContent();
-                    //TODO encrypt
-                    s = s + " hello";
+                    s = encryptString(s);
                     // Sets the text content
                     node.setTextContent(s);
-
-
-
                 }
 
             }
@@ -67,6 +68,7 @@ public class Obfuscator {
             //Once all changes have been made to DOM, write to XML
             TransformerFactory tFactory = TransformerFactory.newInstance();
             Transformer trans = tFactory.newTransformer();
+            trans.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
             DOMSource source = new DOMSource(doc);
             StreamResult result = new StreamResult(new File(pathOfXML));
             trans.transform(source, result);
@@ -83,6 +85,35 @@ public class Obfuscator {
             e.printStackTrace();
         }
 
+    }
+
+    private static String encryptString(String s){
+        // Generate secret key
+        String encryptedString = "";
+        String pass = "password12345678";
+        SecretKeySpec secretKey = new SecretKeySpec(pass.getBytes(), "AES");
+
+        // Encrypt string
+        try {
+            Cipher c = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            c.init(Cipher.ENCRYPT_MODE, secretKey);
+            encryptedString = new String(c.doFinal(s.getBytes("UTF-8"))); // TODO Is this the right way??
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        }
+
+        return encryptedString;
     }
 
     private static void insertDecryptionMethod(){
