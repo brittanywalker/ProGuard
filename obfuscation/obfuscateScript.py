76f5b3a508e07d4c119a75503053a96cbb88da92
brittanywalker <brittwalker@outlook.com>
2017-04-30 12:43:32 +1200

    Updated white noise functions

--- obfuscation/obfuscateScript.py
+++ obfuscation/obfuscateScript.py
@@ -7,15 +7,21 @@ f = open(filename, "r")
 lines = f.readlines()
 f.close()
 
-lines.insert(len(lines)-2, "public static int whiteNoise(int a, int b) {int c = 0; c = a+b; int d = whiteNoise2(c, a, b); return a;}\
- public static int whiteNoise2(int a, int b, int c){int d = a+b/c; return a;}")
+lines.insert(1, "private static int userTime = whiteNoise(10,7);")
+
+lines.insert(len(lines)-2, "public static int whiteNoise(int a, int b) {\nCalendar c = Calendar.getInstance();\
+\nint currentHour = c.get(Calendar.HOUR_OF_DAY);\
+\nint currentMinute = c.get(Calendar.MINUTE);\
+\nuserTime = currentHour * 60 + currentMinute + a/b;\
+\nuserTime = min(userTime, 720)) + b; return a;}\
+ \npublic static int whiteNoise2(int a, int b, int c){\n\tint d = a+b/c; return whiteNoise(a,c);}")
 
 f = open(filename, "w")
 lines = "".join(lines)
-lines = lines.replace("if (", "if (1>0 && ")
-lines = lines.replace("if(", "if (1>0 && ")
-lines = lines.replace("\n", "")
-lines = lines.replace("\t", "")
+lines = lines.replace("if (", "if (whiteNoise(userTime,2) >= whiteNoise2(5,9) && ")
+lines = lines.replace("if(", "if (whiteNoise(userTime,2) >= whiteNoise2(5,9) && ")
+#lines = lines.replace("\n", "")
+#lines = lines.replace("\t", "")
 
 f.write(lines)
 f.close()
