87015a97a0db4f1368d80204a54447a6afaea221
brittanywalker <brittwalker@outlook.com>
2017-04-30 16:54:36 +1200

    White noise bug fixed and obfuscated java files

--- obfuscation/obfuscateScript.py
+++ obfuscation/obfuscateScript.py
@@ -11,16 +11,15 @@ for filename in os.listdir(directory):
         f.close()
 
         pattern = re.compile("^\s*(public|private|protected)\s+\w+\s+\w+[\s*|\w*]*\(")
+        classSig = re.compile("^\s*[\s*|\w*]*(class)[\s*|\w*]*\{") #\s+\w+\s*\{")
         for line, i in zip(lines, range(len(lines))):
-            if line.find("*") < 0 and line.find("class") > -1:
-                lines.insert(i+1, "private static int userTime = whiteNoise(10,7); private static int glass = 8;\
-                private static int drink = 12; private static int hourTime = 60; ")
+            if classSig.match(line):
+                lines.insert(i+1, "private static int userTime = whiteNoise(10,7);\n private static int glass = 8;\n\
+                private static int drink = 12;\n private static int hourTime = 60;\n ")
             if pattern.match(line):
-                lines.insert(i+1, "int a = 8; int variable = whiteNoise(userTime,a);")
+                lines.insert(i+1, "int a = 8;\n int variable = whiteNoise(userTime,a);\n")
 
-
-
-        for j in range(len(lines), 0):
+        for j in range(len(lines)-1, 0, -1):
             if lines[j].find("}") > -1:
                 lines.insert(j, "public static int whiteNoise(int a, int b) {\nCalendar c = Calendar.getInstance();\
                 \nint currentHour = c.get(Calendar.HOUR_OF_DAY);\
@@ -30,14 +29,12 @@ for filename in os.listdir(directory):
                  \npublic static int whiteNoise2(int a, int b, int c){\n\tint d = a+b/c; return whiteNoise(a,c);}")
                 break
 
-
-
         f = open(directory + "/" + filename, "w")
         lines = "".join(lines)
         lines = lines.replace("if (", "if (whiteNoise(userTime,8) >= whiteNoise2(glass,drink,hourTime) && ")
         lines = lines.replace("if(", "if (whiteNoise(userTime,8) >= whiteNoise2(glass,drink,hourTime) && ")
-        #lines = lines.replace("\n", "")
-        #lines = lines.replace("\t", "")
+        lines = lines.replace("\n", "")
+        lines = lines.replace("\t", "")
 
-        f.write(lines)
+        f.write(lines + "\n")
         f.close()
